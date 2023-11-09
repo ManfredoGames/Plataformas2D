ade45a6af7fc78b0aa8e7013164cbb41270a2da8
Marco Di Modica Elias <marcodimodica@protonmail.com>
2023-11-09 20:21:57 +0100

    provisional Item Interaction

---
+++ Plataformas2D/Assets/ItemInteraction.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ItemInteraction : MonoBehaviour
+{
+    public GameObject interactionObject;
+    private bool isPlayerInRange = false;
+
+    public void Interact(InputAction.CallbackContext context)
+    {
+        if (context.performed && isPlayerInRange)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Asegúrate de que tu jugador tenga la etiqueta "Player"
+        {
+            isPlayerInRange = true;
+            interactionObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            interactionObject.SetActive(false);
+        }
+    }
+}
